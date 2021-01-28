15d8a204577542e6c32058465c5293a1c3498771
tungdang214 <tungdang214@gmail.com>
2021-01-27 22:12:41 -0800

    Bug for velocity and stucks

--- enemies.js
+++ enemies.js
@@ -136,40 +136,143 @@ class Skeleton {
 //enemies
 class Zombie {
     constructor(game, x, y) {
-        Object.assign(this,{game, x, y});
-        //spritesheet
-        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/zombies.png");
-
-        // Zombie state variable
-        this.facing = 0; // 0 = right, 1 == left
-        this.state = 0; //0 = idle, 1 = walking, 2 = running, 3= attacking, 4 = die
-        this.dead = false;
+        Object.assign(this, { game, x, y });
+        this.velocity = { x: -PARAMS.BITWIDTH, y: 0 }; // pixels per second
+        console.log("Velocity: ", this.velocity.x);
+        this.animationModes = ["running", "attack", "death"];
+        this.currentMode = this.animationModes[0];
+        this.assetsMap = this.constructAssetMap();
+        this.animations = this.animationModes.map((mode) => this.createZombieAnimator(mode));
+        this.paused = true;
+        this.deadCounter = 0;
+        this.attackCounter = 0;
+        this.updateBoundingBox();
         //Zombie idling
-        this.animation = [];
+        //this.animation = [];
         //idle
-        this.animation[0]= new Animator(this.spritesheet, 146, 15, 35, 40 , 5, 0.33, 60, false, true);
+        //this.animation[0]= new Animator(this.spritesheet, 146, 15, 35, 40 , 5, 0.33, 60, false, true);
         //running
-        this.animation[1]= new Animator(this.spritesheet, 146, 71, 35, 40 , 5, 0.1, 60, false, true);
+       // this.animation[1]= new Animator(this.spritesheet, 146, 71, 35, 40 , 5, 0.1, 60, false, true);
         //hurting
-        this.animation[2] = new Animator(this.spritesheet, 146, 296, 35, 40 , 5, 0.33, 60, false, true);
+        //this.animation[2] = new Animator(this.spritesheet, 146, 122, 35, 40 , 5, 0.1, 60, false, true);
+        //Attacking  
+        //this.animation[2] = new Animator(this.spritesheet, 146, 296, 35, 40 , 5, 0.33, 60, false, true);
+
+    };
+
+    constructAssetMap() {
+        const assetMap = new Map();
+        this.animationModes.forEach((mode) =>
+          assetMap.set(mode, ASSET_MANAGER.getAsset(`./sprites/zombies.png`))
+        );
+        return assetMap;
+    };
+
+    createZombieAnimator(mode) {
+        if (mode == "running"){
+            return new Animator(
+                this.assetsMap.get(mode) ?? "running",
+                146,
+                71,
+                35,
+                40,
+                5,
+                0.1,
+                60,
+                true,
+                mode !== "death"
+              );
+        } else if (mode == "attack") {
+            return new Animator(
+                this.assetsMap.get(mode) ?? "running",
+                146,
+                122,
+                35,
+                40,
+                5,
+                0.1,
+                60,
+                true,
+                mode !== "death"
+              );
+
+        }
 
 
+    }
+
+    updateBoundingBox() {
+        this.lastBB = this.BB;
+        this.BB = new BoundingBox(this.x, this.y, 1.3 * PARAMS.BLOCKWIDTH, 3.1 * PARAMS.BLOCKWIDTH);
+    };
+
+    isAttacking() {
+        return this.currentMode === "attack";
+    }
+
+    isDead() {
+        return this.currentMode === "death";
+    }
+
+    isInCameraView() {
+        return this.game.camera.x > this.x - PARAMS.CANVAS_WIDTH;
+    }
+
+
+
+    update() {
+        if (this.isAttacking()) {
+            this.attackCounter += this.game.clockTick;
+            if (this.attackCounter > 1.6) {
+                this.currentMode = "running";
+                this.attackCounter = 0.0;
+            }
+        }
+        if (this.isDead()) {
+            if (this.deadCounter === 0) {
+                this.game.addEntity(new Score(this.game, this.x, this.y, 100));
+            }
+            this.deadCounter += this.game.clockTick;
+            if (this.deadCounter > 0.5) {
+                this.removeFromWorld = true;
+            }
+        }
+        if (this.paused && this.isInCameraView()) {
+            this.paused = false;
+        }
+        if (!this.paused && !this.isDead()) {
+            this.x += this.game.clockTick * this.velocity.x * PARAMS.SCALE;
+            this.updateBoundingBox();
+            const that = this;
+            this.game.entities.forEach(function (entity) {
+                if (entity.BB && that.BB.collide(entity.BB)) {
+                    if (entity instanceof Sant) {
+                        that.currentMode = "attack";
+                    } else if ((entity instanceof Ground || entity instanceof Brick || entity instanceof Block || entity instanceof Tube)
+                      && that.lastBB.bottom <= entity.BB.top) {
+                        that.y = entity.BB.top - PARAMS.BLOCKWIDTH;
+                        that.updateBoundingBox();
+                    } else if (entity !== that) {
+                        that.velocity.x = -that.velocity.x;
+                    }
+                }
+            });
+        }
+    };
+
+    drawMinimap(ctx, mmX, mmY) {
+        ctx.fillStyle = "Tan";
+        ctx.fillRect(mmX + this.x / PARAMS.BITWIDTH, mmY + this.y / PARAMS.BITWIDTH, PARAMS.SCALE, PARAMS.SCALE);
     };
 
     draw(ctx) {
-        //console.log("hello");
-        //ctx.drawImage(this.spritesheet, 0, 0);
-        this.animation[0].drawFrame(this.game.clockTick, ctx, 0, 0, 2);
-        this.animation[1].drawFrame(this.game.clockTick, ctx, 0, 150, 2);
-        this.animation[2].drawFrame(this.game.clockTick, ctx, 0, 300, 2);
+        this.animations[this.animationModes.indexOf(this.currentMode)]
+          .drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, PARAMS.SCALE)
+        // if (PARAMS.DEBUG) {
+        //     ctx.strokeStyle = 'Red';
+        //     ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y, this.BB.width, this.BB.height);
+        // }
     };
-
-    drawMinimap() {
-
-    }
-
-    update() {
-    }
 
 };
 
